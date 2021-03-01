4e3ad9112a214b3f377a75747e9cfdf83be73858
danascape <saalimquadri1@gmail.com>
2021-03-01 08:40:46 +0530

    ginkgo-sepolicy: fresh start

---
+++ ginkgo-sepolicy.mk
@@ -0,0 +1,7 @@
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/ginkgo-sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/xiaomi/ginkgo-sepolicy/public
+
+# Vendor
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/ginkgo-sepolicy/vendor/xiaomi
