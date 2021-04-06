f5ab1511592d91284381e5eeee52927d3507fc9c
danascape <saalim.priv@gmail.com>
2021-04-06 08:50:28 +0200

    k

---
+++ ginkgo-sepolicy.mk
@@ -0,0 +1,3 @@
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/ginkgo-sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/xiaomi/ginkgo-sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/ginkgo-sepolicy/vendor/xiaomi
